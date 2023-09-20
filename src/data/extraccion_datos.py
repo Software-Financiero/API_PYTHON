9fa7df3623621d4b6aba420ef932f6fecf40dadc
WilcarOrtiz <ortizcolpaswilcardaniel@gmail.com>
2023-09-19 19:57:06 -0500

    extraccion 2

--- src/data/extraccion_datos.py
+++ src/data/extraccion_datos.py
@@ -1,53 +1,90 @@
 import pandas as pd
+import json
 
-
-#API_PYTHON\src\data\LibroExcel.xlsx
-# debes llamar la funcion y pasar la ruta donde esta el libro excel
 
 def extraccion_PIB_excel(ruta):
-    xls = pd.read_excel(ruta, sheet_name="PIB", header=None, names=["Periodo", "PIB","FormaciónBrutaCapital", "Exportaciones","Importaciones"])
-    xls[["Año", "Trimestre"]] = xls["Periodo"].str.split('-', expand=True)
-    xls["PIB"] =xls["PIB"].apply(lambda x: int(x))
-    xls_pib = xls[["Año", "Trimestre", "PIB"]]
+    xls = pd.read_excel(ruta, sheet_name="PIB", header=None, skiprows=2, names=["Periodo", "PIB", "FormaciónBrutaCapital", "Exportaciones", "Importaciones"])
+    trimestre_mapping = {"I": 1, "II": 2, "III": 3, "IV": 4}
+    xls[["Ano", "Trimestre"]] = xls["Periodo"].str.split('-', expand=True)
+    xls["PIB"] = xls["PIB"].apply(lambda x: int(x))
+    xls["Trimestre"] = xls["Trimestre"].str.strip().str.upper().map(trimestre_mapping)
+    xls_pib = xls[["Ano", "Trimestre", "PIB"]]
     diccionario_pib = xls_pib.to_dict(orient='records')
-    return diccionario_pib
+    data_json = json.dumps(diccionario_pib)
+    return data_json
 
 
 def extraccion_IED_excel(ruta):
     xls = pd.read_excel(ruta, sheet_name="IED", header=None, skiprows=2)
-    xls[["Año", "Trimestre"]] = xls[0].str.split('-', expand=True)
+    xls[["Ano", "Trimestre"]] = xls[0].str.split('-', expand=True)
     xls[13] =xls[13].apply(lambda x: int(x))
-    xls_IED = xls[["Año", "Trimestre", 13]]
+    xls = xls.dropna(subset=["Trimestre"])
+    xls_IED = xls[["Ano", "Trimestre", 13]]
     xls_IED= xls_IED.rename(columns={13: "Total"})
     diccionario_IED = xls_IED.to_dict(orient='records')
-    print(diccionario_IED)
+    data_json = json.dumps(diccionario_IED)
+    return data_json
 
 def extraccion_inflacion_excel(ruta):
+    meses = {
+        1: "Ene",
+        2: "Feb",
+        3: "Mar",
+        4: "Abr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Ago",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dic"
+    }
+    
     xls = pd.read_excel(ruta, sheet_name="INFLACION")
     xls_inflacion = xls[["Año", "Mes", "Porcentaje"]]
+    xls_inflacion = xls_inflacion.rename(columns={"Año": "Ano"})
+    xls_inflacion["Mes"] = xls_inflacion["Mes"].map(meses)
     inflacion_dict = xls_inflacion.to_dict(orient="records")
-    return inflacion_dict
+    data_json = json.dumps(inflacion_dict)
+    return data_json
     
 def extraccion_desempleo_excel(ruta):
     xls = pd.read_excel(ruta, sheet_name="DESEMPLEO", header=None, skiprows=2)
     xls[11] = xls[11].round(2)
     xls = xls.dropna(axis=1, how='any')
     selected_columns = xls[[0, 1, 11]]
-    selected_columns.columns = ["Año", "Mes", "Tasa"]
+    selected_columns.columns = ["Ano", "Mes", "Tasa"]
     desempleo_dict = selected_columns.to_dict(orient='records')
-    return desempleo_dict
-    
+    data_json = json.dumps(desempleo_dict)
+    return data_json
     
 def extraccion_deuda_excel(ruta):
+    meses = {
+        1: "Ene",
+        2: "Feb",
+        3: "Mar",
+        4: "Abr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Ago",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dic"
+    }
+    
     xls = pd.read_excel(ruta, sheet_name="DEUDA", header=None)
     xls= xls[xls[1] == 2]
     # Convierte la columna 0 en formato datetime
     xls[0] = pd.to_datetime(xls[0])
-    xls['Año'] = xls[0].dt.year
+    xls['Ano'] = xls[0].dt.year
     xls['Mes'] = xls[0].dt.month
     xls['Dia'] = xls[0].dt.day
-    
-    xls_deuda= xls[["Año", "Mes", "Dia",4]]
+    xls["Mes"] = xls["Mes"].map(meses)
+    xls_deuda= xls[["Ano", "Mes", "Dia",4]]
     xls_deuda = xls_deuda.rename(columns={4: "total"})
     deuda_dict = xls_deuda.to_dict(orient="records")
-    return deuda_dict
+    data_json = json.dumps(deuda_dict)
+    return data_json
